760ac13dbf8bd70fd5de845fbfd422afa110883b
Ryan Bressette <bressetteryan@gmail.com>
2020-06-04 12:54:09 -0400

    Add controller to handle circuit requests.

---
+++ LogicGateSimOnline/Controllers/CircuitController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LogicGateDomain;
+using FCircuitParser;
+
+namespace LogicGateSimOnline.Controllers
+{
+    [Route("api/[controller]")]
+    public class CircuitController : Controller
+    {
+        [HttpPost("[action]")]
+        public IEnumerable<LogicGate> BuildCircuit([FromBody]CircuitProgram program)
+        {
+            var statements = FCircuitParser.Parser.getProgramFromString(program.Text);
+            var circuit = FCircuitParser.Parser.getCircuit(statements);
+            return circuit;
+        }
+
+        public class CircuitProgram
+        {
+            public string Text { get; set; }
+        }
+        
+    }
+}
